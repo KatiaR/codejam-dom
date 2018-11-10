202a6957386bb7455b5dac4ba5015c3ed3424658
Екатерина Романюк <katia-l@mail.ru>
2018-11-11 01:38:29 +0300

    Feat: add function to slider in the modal window

--- codejam-dom/src/index.js
+++ codejam-dom/src/index.js
@@ -16,5 +16,33 @@ function onLoad() {
     console.log(stateDisableTips.checked);
     localStorage.setItem('stateDisableTips', stateDisableTips.checked);
   });
+
+  const arraySlides = [
+    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    'Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+    'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+    'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout',
+    'Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like',
+    'All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. ',
+  ];
+  const btnNext = document.querySelector('.next');
+  const btnPrev = document.querySelector('.prev');
+  const slider = document.querySelector('.text-slides');
+
+  let i = 0;
+  btnNext.addEventListener('click', () => {
+    slider.innerHTML = arraySlides[i];
+    i += 1;
+    if (i >= arraySlides.length - 1) {
+      i = 0;
+    }
+  });
+  btnPrev.addEventListener('click', () => {
+    slider.innerHTML = arraySlides[i];
+    i -= 1;
+    if (i <= 0) {
+      i = arraySlides.length - 1;
+    }
+  });
 }
-window.onload = setTimeout(onLoad, 5000);
+window.onload = setTimeout(onLoad, 1000);
