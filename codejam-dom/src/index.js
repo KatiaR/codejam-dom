f249ff4b7998062951523ac50fc3fbd6dd0cd3ea
Екатерина Романюк <katia-l@mail.ru>
2018-11-11 00:33:28 +0300

    feat: add opportunities to modal window

--- codejam-dom/src/index.js
+++ codejam-dom/src/index.js
@@ -1,3 +1,20 @@
 import './scss/main.scss';
 
-alert('hello');
+function onLoad() {
+  const modalWindow = document.querySelector('.modal-window');
+  console.log(localStorage.getItem('stateDisableTips'));
+  if (!localStorage.getItem('stateDisableTips')) {
+    console.log('stateDisableTips');
+    modalWindow.classList.remove('closed');
+  }
+  const btnClose = document.querySelector('.close');
+  btnClose.addEventListener('click', () => {
+    modalWindow.classList.add('closed');
+  });
+  const stateDisableTips = document.getElementById('tips');
+  stateDisableTips.addEventListener('click', () => {
+    console.log(stateDisableTips.checked);
+    localStorage.setItem('stateDisableTips', stateDisableTips.checked);
+  });
+}
+window.onload = setTimeout(onLoad, 5000);
