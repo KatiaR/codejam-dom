404e15111cac9b79a328726dc6bfbadfc5b364ed
Екатерина Романюк <katia-l@mail.ru>
2018-11-11 12:45:08 +0300

    feat: add slider to modal window

--- codejam-dom/src/index.js
+++ codejam-dom/src/index.js
@@ -1,6 +1,8 @@
 import './scss/main.scss';
 
 function onLoad() {
+  let i = 0;
+
   const modalWindow = document.querySelector('.modal-window');
   console.log(localStorage.getItem('stateDisableTips'));
   if (!localStorage.getItem('stateDisableTips')) {
@@ -17,32 +19,52 @@ function onLoad() {
     localStorage.setItem('stateDisableTips', stateDisableTips.checked);
   });
 
+  function setActiveDot() {
+    const dots = document.querySelectorAll('.dot');
+    console.log(i);
+    [...dots].forEach((item, index) => {
+      item.classList.remove('active');
+      if (index === i) {
+        item.classList.add('active');
+      }
+    });
+  }
+  setActiveDot();
+
   const arraySlides = [
     'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     'Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
     'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
     'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout',
     'Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like',
-    'All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. ',
   ];
   const btnNext = document.querySelector('.next');
   const btnPrev = document.querySelector('.prev');
   const slider = document.querySelector('.text-slides');
 
-  let i = 0;
   btnNext.addEventListener('click', () => {
-    slider.innerHTML = arraySlides[i];
     i += 1;
-    if (i >= arraySlides.length - 1) {
+    if (i > arraySlides.length - 1) {
       i = 0;
     }
+    slider.innerHTML = arraySlides[i];
+    setActiveDot();
   });
   btnPrev.addEventListener('click', () => {
-    slider.innerHTML = arraySlides[i];
     i -= 1;
-    if (i <= 0) {
+    if (i < 0) {
       i = arraySlides.length - 1;
     }
+    slider.innerHTML = arraySlides[i];
+    setActiveDot();
+  });
+  const dotSlider = document.querySelector('.dots');
+  dotSlider.addEventListener('click', (e) => {
+    const indexDot = e.target.getAttribute('data-position');
+    if (indexDot === null) return;
+    i = +e.target.getAttribute('data-position');
+    slider.innerHTML = arraySlides[i];
+    setActiveDot();
   });
 }
 window.onload = setTimeout(onLoad, 1000);
